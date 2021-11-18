9ad075385840d8d5447ad06790522e8babe3e1f5
hallefcalves <48840344+hallefcalves@users.noreply.github.com>
2021-11-18 20:28:02 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,7 @@ if (environment.production) {
   enableProdMode();
 }
 
+.listen(process.env.PORT || 5000)
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
