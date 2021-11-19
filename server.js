602df413a597ee6acab7b40f4303762a3bda67fb
hallefcalves <hallef28@gmail.com>
2021-11-19 15:08:57 -0300

    Acerto deploy

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/speech-to-libras'));
+app.use(express.static('./dist/IBOOK-FRONT'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/speech-to-libras/'}),
+    res.sendFile('index.html', {root: 'dist/IBOOK-FRONT/'}),
 );
 
 // Start the app by listening on the default Heroku port
